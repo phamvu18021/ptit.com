0aa708b83b32df8e885560ad8043fae4a0d8ac3e
phamvu18021 <92640554+phamvu18021@users.noreply.github.com>
2023-10-24 10:40:42 +0700

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -5,31 +5,30 @@ export const menus = [
   },
 
   {
-    path: "#", // the url
+    path: "/nganh-hoc", // the url
     title: "NGÀNH HỌC",
-    childs: [
-      {
-        path: "/nganh-cong-nghe-thong-tin",
-        title: "Công nghệ thông tin",
-      },
-      {
-        path: "/nganh-dien-tu-vien-thong",
-        title: "Điện tử viễn thông",
-      },
-      {
-        path: "/nganh-quan-tri-kinh-doanh",
-        title: "Quản trị kinh doanh",
-      },
-    ],
+    // childs: [
+    //   {
+    //     path: "/nganh-cong-nghe-thong-tin",
+    //     title: "Công nghệ thông tin",
+    //   },
+    //   {
+    //     path: "/nganh-dien-tu-vien-thong",
+    //     title: "Điện tử viễn thông",
+    //   },
+    //   {
+    //     path: "/nganh-quan-tri-kinh-doanh",
+    //     title: "Quản trị kinh doanh",
+    //   },
+    // ],
   },
 
   {
-    path: "/Tuyen-sinh", // the url
+    path: "/tuyen-sinh", // the url
     title: "TUYỂN SINH",
   },
   {
     path: "/tin-tuc", // the url
     title: "TIN TỨC & SỰ KIỆN", // view rendered
   },
-
 ];
